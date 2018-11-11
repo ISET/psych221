33ca3cdf5fe6bb440b23999cbbdb53fe62802d4e
Brian Wandell <wandell@stanford.edu>
2018-11-11 12:57:31 -0800

    Creating simulated data for training and the class project

---
+++ trainingdata/ie_prIR2RGB.m
@@ -0,0 +1,84 @@
+%% L3:  Transform an IR (monochrome) image to a colorized RGB
+%
+% Choose a natural scene that extends to IR
+%  Make a monochrome sensor with an IR only filter for a particular lens and sensor
+%  Make a matched RGB sensor image with the same lens
+%  Render the image on the two different sensors
+%  Store the matched sensor data
+%
+% Possibe approaches
+%  Transform patches of high resolution IR data to a 2x2 RGB superpixel
+%  
+%
+% BW, 2018
+
+
+%% Basic parameters to keep things organized
+
+wave = 415:10:940;
+fov = 10;
+
+%% Create a matched IR and RGB camera
+
+% Start with an rgb camera
+rgbSensor = sensorCreate;
+rgbSensor = sensorSetSizeToFOV(rgbSensor,fov);
+rgbSensor = sensorSet(rgbSensor,'wave',wave);
+rgbSensor = sensorSet(rgbSensor,'name','RGB');
+
+% Build the irSensor.  Matched except uses the irPassFilter.
+irSensor = sensorSet(rgbSensor,'pattern',1);
+irFname  = 'irPassFilter';
+irPass   = ieReadColorFilter(wave,irFname);
+irSensor = sensorSet(irSensor,'color filters',irPass);
+irSensor = sensorSet(irSensor,'filter names',{'kIR'});  % Shows up as black.
+irSensor = sensorSet(irSensor,'name','IR');
+
+%% The stanford hyperspectral data on the RDT site contain data to 950
+
+% These are in several directories
+%   fruit, landscape, faces1m and faces3m
+% 
+% The data are large, with densely sampled wavelength
+% We reduce the sampling to make the computations faster.  No real loss of
+% precision, IMHO.
+%
+
+rdt.crp('/resources/scenes/hyperspectral/stanford_database/fruit'); % change remote path
+a = rdt.listArtifacts('type','mat','print',true);  
+
+data  = rdt.readArtifacts(a(5));
+
+scene = sceneFromBasis(data{1});
+scene = sceneInterpolateW(scene,wave);
+
+oi = oiCreate;
+oi = oiCompute(oi,scene);
+
+%% 
+
+rgbSensor = sensorCompute(rgbSensor,oi);
+irSensor = sensorCompute(irSensor,oi);
+
+% the oiCompute only needs to be run once.
+ieAddObject(rgbSensor); sensorWindow;
+ieAddObject(irSensor); sensorWindow;
+
+%%  2002 and 2004 from Manchester are only out to 750.
+
+rdt.crp('/resources/scenes/hyperspectral/manchester_database/2004'); % change remote path
+a = rdt.listArtifacts('type','mat','print',true);
+
+data  = rdt.readArtifacts(a(2));
+
+scene = sceneFromBasis(data{1});
+ieAddObject(scene); sceneWindow;
+
+%% Show them image processed
+
+ip = ipCreate;
+ipIR = ipCompute(ip,irSensor);
+ipRGB = ipCompute(ip,rgbSensor);
+ieAddObject(ipIR); ieAddObject(ipRGB); ipWindow;
+
+%% END
---
+++ trainingdata/ie_prLowHigh.m
@@ -0,0 +1,65 @@
+%% L3:  Transform low to high resolution data using machine learning (e.g., L3)
+%
+% Choose a high resolution natural scene as a first example. 
+% Make a sensor image for a particular lens and sensor
+% Render the sensor data
+% Double the sensor spatial resolution and render again
+% Store the matched sensor data
+%
+% Possibe approaches
+%   Transform each of the low resolution pixels to a 2x2 superpixel
+%   Transform each of the 2x2 low resolution super pixels to a 4x4 high
+%   resolution super pixel
+%   Render these data using an ip object and learn based on the RGB images
+%
+% Software dependencies
+%   ISETCam and RDT
+%
+% BW, 2018
+
+%%
+ieInit;
+fov = 5;
+
+%%  Figure out some scenes on the RDT
+% The data in 2009 go out to 950nm
+rdt.crp('/resources/scenes/multiband/scien/2008'); % change remote path
+
+% rdt.listRemotePaths
+a = rdt.listArtifacts('type','mat','print',true);
+%%
+data = rd.readArtifacts(a(1));
+scene = sceneFromBasis(data{1});
+
+% Treat the scene as small
+scene = sceneSet(scene,'fov',fov);
+ieAddObject(scene); sceneWindow;
+
+oi = oiCreate;
+oi = oiCompute(oi,scene);
+ieAddObject(oi); oiWindow;
+
+%% Create a low (sensorL) and high (sensorH) resolution sensor
+
+sensor = sensorCreate;
+sz = sensorGet(sensor,'pixel size');  % Row Col
+sensorL = sensorSetSizeToFOV(sensor,fov);
+
+% Make the pixel half the size
+sensorH = sensorSet(sensorL,'pixel size same fill factor',sz(1)/2);
+sensorH = sensorSetSizeToFOV(sensorH,fov);
+
+%%  Compute the low and high resolution sensor images
+
+sensorH = sensorCompute(sensorH,oi);
+sensorL = sensorCompute(sensorL,oi);
+ieAddObject(sensorH); sensorWindow;
+ieAddObject(sensorL); sensorWindow;
+
+%%  To see what the data look like after image processing
+
+ip = ipCreate;
+ipH = ipCompute(ip,sensorH);
+ipL = ipCompute(ip,sensorL);
+ieAddObject(ipH); ieAddObject(ipL); ipWindow;
+
