6fab27e1f8b0a8b8f028e818af70efe87c7cde8c
Brian Wandell <wandell@stanford.edu>
2019-11-11 22:46:13 -0800

    Fixed for ISO calculation of MTF

--- projects/isetbioEyeMovements.m
+++ projects/isetbioEyeMovements.m
@@ -19,15 +19,13 @@
 ieInit
 
 %%  The slanted bar scene
-scene = sceneCreate('slanted bar');
+scene = sceneCreate('slanted edge');
 
 %%  A standard human eye ball
-
 oi = oiCreate;
 oi = oiCompute(oi,scene);
 
 %% Rectangular mosaic
-
 cones = coneMosaic;
 cones.spatialDensity = [0,1,0,0];   % Only L cones
 cones.integrationTime = 0.001;      % One ms integration time
@@ -36,19 +34,41 @@ cones.emGenSequence(50);            % 50 eye movements (50 ms)
 %%
 cones.compute(oi);
 
-%%
+%% Not needed, but nice to see
 cones.window;
 
 %%  Get the mean absorptions across time
-
 absorptions = cones.absorptions;
-meanA = mean(absorptions,3);
-ieNewGraphWin; imagesc(meanA); 
+edgeImage = mean(absorptions,3);
+% ieNewGraphWin; imagesc(edgeImage);
 
-rgb = repmat(meanA,1,1,3);
+% ISO12233 requires cropping the image so that the dge is taller than wide
+% This is a specific way to do it.  But we need a more general way.
+sz = size(edgeImage);
+xmin = 30;
+ymin= 15;
+width  = 35;
+height = 50;
+
+edgeImage = imcrop(edgeImage,[xmin ymin width height]);
+ieNewGraphWin; imagesc(edgeImage); axis image; colormap(gray);
+
+%% Call the ISO routine
+
+% It returns the key parameters
 deltaX = cones.patternSampleSize(1)*1e3;
-ISO12233(rgb,deltaX,[1 0 0]);
+mtf = ISO12233(edgeImage,deltaX,[1 0 0]);
 colormap(gray); colorbar
+
+%% Here is a plot only up to the Nyquist sampling frequency
+ieNewGraphWin;
+keep = mtf.freq < mtf.nyquistf;
+plot(mtf.freq(keep),mtf.mtf(keep),'k-','linewidth',2);
+grid on;
+xlabel('Cycles/mm');
+ylabel('Contrast reduction');
+
+%%
 
 
 
