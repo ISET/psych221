fae6e7661c9b6fcb069128a3e799ad2c64b00b84
Brian Wandell <wandell@stanford.edu>
2019-12-14 21:55:38 -0800

    Root path

---
+++ teachRootPath.m
@@ -0,0 +1,15 @@
+function rootPath=teachRootPath()
+% Return the path to the root iset directory
+%
+% This function must reside in the directory at the base of the ISET
+% directory structure.  It is used to determine the location of various
+% sub-directories.
+% 
+% Example:
+%   fullfile(isetRootPath,'data')
+
+rootPath = which('teachRootPath');
+
+[rootPath,fName,ext]=fileparts(rootPath);
+
+return
