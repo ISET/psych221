372efac2da7d0a6fa5a4a86218226cf2e0a016b9
Brian Wandell <wandell@stanford.edu>
2022-10-04 17:39:07 -0700

    paraxial slides

--- slides/paraxialApproximations.m
+++ slides/paraxialApproximations.m
@@ -1,17 +1,72 @@
 %% Paraxial approximations
+%
+% For making the paraxial geometric optics slides
+%
 
-x = [-pi:0.1:pi];
+%% 
+chdir(teachRootPath);
+chdir('slides');
+
+%% 
+close all
+
+%% 0.1 radian is about 5.7 deg
+
+xRadians = -1:0.01:1;
+xDegrees = rad2deg(xRadians);
+lst = xDegrees > -20 & xDegrees < 20;
+xRange = deg2rad(xDegrees(lst));
+
 ieNewGraphWin;
-plot(x,sin(x));
-hold on;
-plot(x,x,'k--');
+plot(xRadians,sin(xRadians),'LineWidth',2);
+hold on; plot(xRadians,xRadians,'k--','LineWidth',2);
 grid on;
+xlabel('Angle (radians)')
+set(gca,'ylim',[-1 1])
+line([0.2 0.2],[-1 1],'Color',[0.3 0.3 0.3]);
+line([-0.2 -0.2 ],[-1 1 ],'Color',[0.3 0.3 0.3]);
+xlabel('Angle (radians)');
+exportgraphics(gca,'paraxSine.png','Resolution',150);
 
 ieNewGraphWin;
-plot(x,cos(x));
-hold on; plot(x,ones(size(x)),'k--');
+plot(xRadians,cos(xRadians),'LineWidth',2);
+hold on; plot(xRadians,ones(size(xRadians)),'k--','LineWidth',2);
+grid on;
+set(gca,'ylim',[-1 1])
+line([0.2 0.2],[-1 1],'Color',[0.3 0.3 0.3]);
+line([-0.2 -0.2 ],[-1 1 ],'Color',[0.3 0.3 0.3]);
+xlabel('Angle (radians)');
+exportgraphics(gca,'paraxCosine.png','Resolution',150);
 
 ieNewGraphWin;
-plot(x,tan(x),'')
-hold on; plot(x,x,'k--');
+plot(xRadians,tan(xRadians),'LineWidth',2)
+hold on; plot(xRadians,xRadians,'k--','LineWidth',2); 
+grid on;
+set(gca,'ylim',[-1 1])
+line([0.2 0.2],[-1 1],'Color',[0.3 0.3 0.3]);
+line([-0.2 -0.2 ],[-1 1 ],'Color',[0.3 0.3 0.3]);
+xlabel('Angle (radians)');
+exportgraphics(gca,'paraxTangent.png','Resolution',150);
 
+%%
+%{
+ieNewGraphWin;
+plot(xRange,sin(xRange));
+hold on; plot(xRange,xRange,'k--');
+grid on;
+xlabel('Angle (radians)')
+set(gca,'ylim',[-1 1])
+
+ieNewGraphWin;
+plot(xRange,cos(xRange));
+hold on; plot(xRange,ones(size(xRange)),'k--');
+grid on;
+set(gca,'ylim',[0 1])
+
+ieNewGraphWin;
+plot(xRange,tan(xRange),'')
+hold on; plot(xRange,xRange,'k--'); 
+grid on;
+set(gca,'ylim',[-1 1])
+%}
+%%
